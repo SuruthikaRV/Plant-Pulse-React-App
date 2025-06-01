c6a1aff234aae87d0a3c94b0d14ad84cff76e07f
SuruthikaRV <147537240+SuruthikaRV@users.noreply.github.com>
2025-06-01 20:56:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-const mongoURI = 'mongodb+srv://suruthika:kZSgoR6gL0WXnBJv@gardencluster.zs7oi.mongodb.net/?retryWrites=true&w=majority&appName=GardenCluster'; // Replace with your MongoDB connection string
+const mongoURI = 'YOUR_CONNECTION_STRING'; // Replace with your MongoDB connection string
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected successfully!'))
   .catch(err => console.error('MongoDB connection error:', err));
